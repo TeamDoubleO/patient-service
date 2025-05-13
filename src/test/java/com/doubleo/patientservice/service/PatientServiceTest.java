969e9f7f40bc14bad7b6df96e9013424084c367e
midday2612 <midday2612@gmail.com>
2025-05-13 12:33:22 +0900

    KW-344/fix: test 코드에 보호자 필드 추가

--- src/test/java/com/doubleo/patientservice/service/PatientServiceTest.java
+++ src/test/java/com/doubleo/patientservice/service/PatientServiceTest.java
@@ -16,6 +16,7 @@ import com.doubleo.patientservice.global.exception.CommonException;
 import com.doubleo.patientservice.global.exception.errorcode.PatientErrorCode;
 import com.doubleo.patientservice.global.util.TenantValidator;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
@@ -47,7 +48,8 @@ public class PatientServiceTest {
                         "001111-1234567",
                         Sex.MALE,
                         LocalDateTime.of(2025, 5, 9, 16, 30),
-                        1L);
+                        1L,
+                        new ArrayList<>());
         ReflectionTestUtils.setField(patient, "id", 1L);
         ReflectionTestUtils.setField(patient, "tenantId", "SEO25NE01");
     }
