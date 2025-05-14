a0b2b9c3d2b385e9012446cfca90d4a74ded36ad
midday2612 <midday2612@gmail.com>
2025-05-14 17:56:02 +0900

    KW-363/feat: 보호자 조회 controller 수정

--- src/main/java/com/doubleo/patientservice/domain/guardian/controller/GuardianMobileController.java
+++ src/main/java/com/doubleo/patientservice/domain/guardian/controller/GuardianMobileController.java
@@ -1,9 +1,11 @@
 package com.doubleo.patientservice.domain.guardian.controller;
 
+import com.doubleo.patientservice.domain.guardian.dto.request.GuardianInfoRequest;
 import com.doubleo.patientservice.domain.guardian.dto.response.GuardianInfoResponse;
 import com.doubleo.patientservice.domain.guardian.service.GuardianService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -16,10 +18,10 @@ public class GuardianMobileController {
     private final GuardianService guardianService;
 
     @Operation(summary = "환자 별 보호자 조회", description = "모바일에서 환자가 자신의 보호자를 조회")
-    @GetMapping("/guardians")
-    public List<GuardianInfoResponse> getGuardiansByPatientId(
-            @RequestHeader("X-Patient-Code") String patientCode,
+    @PostMapping("/guardians")
+    public List<GuardianInfoResponse> guardiansGetByPatientId(
+            @Valid @RequestBody GuardianInfoRequest request,
             @RequestHeader("X-Hospital-Id") Long hospitalId) {
-        return guardianService.getGuardiansByPatientCode(patientCode);
+        return guardianService.getGuardiansByPatientCode(request.patientCode());
     }
 }
